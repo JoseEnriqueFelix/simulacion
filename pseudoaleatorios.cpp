a59065c590fa951ad5407c93a824283565537e00
Jose Felix <joseenrfelixesp@gmail.com>
2023-03-12 19:58:24 -0700

    Segun lo que decia mi tilina

--- pseudoaleatorios.cpp
+++ pseudoaleatorios.cpp
@@ -32,30 +32,37 @@ double calcvarianza(int arr[], int n, double media){
 
 int main(){
     cout << setprecision(15);
-    cout << "Ingrese la cantidad de numeros pseudoaleatorios a generar: " << endl;
-    int n;
-    cin >> n;
-    int arr[n];
+    int n1;
     int seed=time(0);
+    int n2=time(0)%10000;
     bool menu;
     cout << "========================================" << endl;
-    cout << "Ingrese 1 si desea generarlos usted mismo" << endl;
-    cout << "Ingrese 0 si desea generarlos por computadora" << endl;
+    cout << "Ingrese 1 si desea generarlos por teclado" << endl;
+    cout << "Ingrese 0 si desea generarlos de manera aleatoria" << endl;
     cin >> menu;
     if(menu){
-        for(int i=0; i<n; i++){
-            cout << "Ingrese el numero " << (i+1) << endl;
-            cin >> arr[i];
-        }
-    }
-    else{
-        for(int i=0; i<n; i++){
+        cout << "Ingrese la cantidad de numeros aleatorios a generar" << endl;
+        cin >> n1;
+        int arr[n1];
+        for(int i=0; i<n1; i++){
             arr[i]=pseudoaleat((i+13*54) * seed);
             cout << arr[i] << endl;
         }
+        cout << "========================================" << endl;
+        double media=calcmedia(arr, n1);
+        cout << "La media es: " << media << endl;
+        cout << "La varianza es: " << calcvarianza(arr, n1, media) << endl;
     }
-    cout << "========================================" << endl;
-    cout << "La media es: " << calcmedia(arr, n) << endl;
-    cout << "La varianza es: " << calcvarianza(arr, n, calcmedia(arr, n)) << endl;
+    else{
+        int arr[n2];
+        for(int i=0; i<n2; i++){
+            arr[i]=pseudoaleat((i+13*54) * seed);
+            cout << arr[i] << endl;
+        }
+        cout << "La cantidad de numeros aleatorios que fueron generados es de " << n2 << " numeros" << endl;
+        double media=calcmedia(arr, n2);
+        cout << "La media es: " << media << endl;
+        cout << "La varianza es: " << calcvarianza(arr, n2, media) << endl;
+    }
     return 0;
 }
